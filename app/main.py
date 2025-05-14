a7c8bfbae3a6dbf9edd3d54c58b9695a9469572d
Francisco Chairiglione <fchiariglione@gmail.com>
2025-05-14 19:00:30 -0300

    agrega post

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse, Response
+from pydantic import BaseModel, Field
+from typing import List, Dict, Optional, Tuple, Union
 from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 import logging
 import json
@@ -16,6 +18,52 @@ REQUEST_COUNT = Counter(
 REQUEST_LATENCY = Histogram(
     'pokemon_request_latency_seconds', 'Latency of pokemon endpoint', ['endpoint']
 )
+
+# === Models ===
+class PokemonName(BaseModel):
+    english: str
+    japanese: str
+    chinese: str
+    french: str
+
+
+class PokemonStats(BaseModel):
+    HP: int
+    Attack: int
+    Defense: int
+    Sp_Attack: int = Field(..., alias="Sp. Attack")
+    Sp_Defense: int = Field(..., alias="Sp. Defense")
+    Speed: int
+
+
+class PokemonEvolution(BaseModel):
+    next: List[List[Union[str, int]]]
+
+
+class PokemonProfile(BaseModel):
+    height: str
+    weight: str
+    egg: List[str]
+    ability: List[List[Union[str, str]]]
+    gender: str
+
+
+class PokemonImage(BaseModel):
+    sprite: str
+    thumbnail: str
+    hires: str
+
+
+class Pokemon(BaseModel):
+    id: int
+    name: PokemonName
+    type: List[str]
+    base: PokemonStats
+    species: str | None = None
+    description: str | None = None
+    evolution: PokemonEvolution | None = None
+    profile: PokemonProfile | None = None
+    image: PokemonImage | None = None
 
 # Load data
 DATA_PATH = os.path.join(os.path.dirname(__file__), "pokedex.json")
@@ -53,6 +101,24 @@ def get_pokemon_by_pokedex_nro(id: int):
         logging.warning(f"Pokemon Nro {id} not found")
         logging.info(f"End request for /pokedex/{id} with 404")
         raise HTTPException(status_code=404, detail="Pokemon not found")
+    
+@app.post("/pokemon", response_model=Pokemon, status_code=201)
+def create_pokemon(pokemon: Pokemon):
+    logging.info("Start request for POST /pokemon")
+    with REQUEST_LATENCY.labels(endpoint='/pokemon').time():
+        existing_ids = {p["id"] for p in pokemons}
+        if pokemon.id in existing_ids:
+            REQUEST_COUNT.labels(method='POST', endpoint='/pokemon', status_code='400').inc()
+            logging.warning(f"Pokemon ID {pokemon.id} already exists")
+            raise HTTPException(status_code=400, detail="Pokemon ID already exists")
+
+
+        pokemon_dict = pokemon.dict(by_alias=True)
+        pokemons.append(pokemon_dict)
+
+        logging.info(f"Pokemon created: {pokemon.name.english}")
+        REQUEST_COUNT.labels(method='POST', endpoint='/pokemon', status_code='201').inc()
+        return pokemon
 
 @app.get('/metrics')
 def metrics():
