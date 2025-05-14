ce083267ec0bcf78930d837fd53a0f85e577af0b
Francisco Chairiglione <fchiariglione@gmail.com>
2025-05-14 16:11:06 -0300

    endpoint nro pokedex

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,14 @@ def test_get_existing_pokemon():
 def test_get_nonexistent_pokemon():
     response = client.get("/pokemon/mewtwo")
     assert response.status_code == 404
+
+def test_get_existing_pokedex():
+    response = client.get("/pokedex/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == 1
+
+
+def test_get_nonexistent_pokedex():
+    response = client.get("/pokemon/999")
+    assert response.status_code == 404
