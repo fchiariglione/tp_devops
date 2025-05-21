c7b5744b81ff7df074374c2c19f5eab0911f5949
Francisco Chairiglione <fchiariglione@gmail.com>
2025-05-21 20:52:12 -0300

    fix tests

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ import os
 app = FastAPI()
 
 # Configure logging
-log_dir = "/app/logs"
-os.makedirs(log_dir, exist_ok=True)  # Ensure log directory exists
+log_dir = os.path.join(os.path.dirname(__file__), "logs")
+os.makedirs(log_dir, exist_ok=True)
 
 log_file_path = os.path.join(log_dir, "app.log")
 
